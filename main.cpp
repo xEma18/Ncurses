77a6ac67ffd9674330e42b8f41b971dc2ce8df83
Emanuele Di Giacomo <emanueledg44@gmail.com>
2022-03-29 11:33:10 +0200

    many progesses made

--- main.cpp
+++ main.cpp
@@ -66,9 +66,14 @@ struct room{
   WINDOW *win;
   int max_x;
   int max_y;
+  int start_door;
+  int end_door;
   wall *walls;
+  room *next;
+  room *prev;
   // da aggiungere: nemici, artefatti ecc...
 };
+typedef room *p_room;
 
 void stampa_muri(wall *walls, WINDOW *win){
   while(walls != NULL){
@@ -84,7 +89,7 @@ void stampa_muri(wall *walls, WINDOW *win){
     }
     walls = walls->next;
   }
-}
+} // funzione che stampa i muri
 
 class Personaggio{
 protected:
@@ -92,15 +97,15 @@ protected:
   int current_y;
   int max_y;
   int max_x;
-  WINDOW *win;
+  room *room_head;
 public:
-  Personaggio(WINDOW *win, int max_y, int max_x, int current_x = 0, int current_y = 0){
+  Personaggio(room *head, int max_y, int max_x, int current_x = 1, int current_y = 1){
     this->max_y = max_y;
     this->max_x = max_x;
     this->current_x = current_x;
     this->current_y = current_y;
-    this->win = win;
-    this->update();
+    this->room_head = head;
+    this->init();
   }
 
   bool check_for_h_walls(wall *head, int direction){    // the "h" stands for "horizontal"
@@ -150,31 +155,35 @@ public:
   // bool check_for_v_walls(wall *head, int direction);
 
   void move_up(wall *head){
-    if(this->current_y > 1 && this->check_for_h_walls(head, 1)){ // a function that checks wheter there's a wall or not  wether
+    if(this->current_y > 1 && this->check_for_h_walls(room_head->walls, 1)){ // a function that checks wheter there's a wall or not  wether
       this->current_y = this->current_y - 1;
     }
   }
 
   void move_down(wall *head){
-    if(this->current_y < max_y - 2 && check_for_h_walls(head, 2)){
+    if(this->next_room()){
+      this->current_y += 1;
+      this->go_ahead();
+    }
+    else if(this->current_y < max_y - 2 && check_for_h_walls(room_head->walls, 2)){
       this->current_y = this->current_y + 1;
     }
   }
 
   void move_left(wall *head){
-    if(this->current_x > 1 && check_for_h_walls(head, 3)){
+    if(this->current_x > 1 && check_for_h_walls(room_head->walls, 3)){
       this->current_x = this->current_x - 1;
     }
   }
 
   void move_right(wall *head){
-    if(this->current_x < max_x - 2 && check_for_h_walls(head, 4)){
+    if(this->current_x < max_x - 2 && check_for_h_walls(room_head->walls, 4)){
       this->current_x = this->current_x + 1;
     }
   }
 
   void move(int direction, wall *head){
-    mvwprintw(win, current_y, current_x, " ");
+    mvwprintw(room_head->win, current_y, current_x, " ");
     switch(direction){
       case 1: this->move_up(head); break;
       case 2: this->move_down(head); break;
@@ -185,9 +194,45 @@ public:
     this->update();
   }
 
+  bool next_room(){
+    if((this->current_x >= room_head->start_door && this->current_x <= room_head->end_door) && (this->current_y + 1 == room_head->max_y-1 )){
+      return true;
+    }
+    return false;
+  }
+
   void update(){
-    mvwprintw(win, current_y, current_x, "P");
-    wrefresh(this->win);
+    mvwprintw(room_head->win, current_y, current_x, "P");
+    wrefresh(room_head->win);
+    refresh();
+  }
+
+  void go_ahead(){
+    this->room_head = this->room_head->next;
+    this->init();
+  }
+
+  void go_backwards(){
+    this->room_head = this->room_head->prev;
+    this->init();
+  }
+
+  void reset(){
+    this->current_x = 1;
+    this->current_y = 1;
+  }
+
+  void init(){
+    clear();
+    stampa_muri(room_head->walls, room_head->win);
+    refresh();
+    reset();
+    box(room_head->win, 0, 0);
+    for(int i = room_head->start_door; i <= room_head->end_door; i++){
+      mvwprintw(room_head->win, max_y-1, i, " ");
+    }
+    wrefresh(this->room_head->win);
+    update();
   }
 };
 
@@ -197,6 +242,7 @@ int main(){
   curs_set(0);
 
   WINDOW *win = newwin(20, 40, 5, 10);
+  WINDOW *win2 = newwin(20, 40, 5, 10);
 
   wall *head = new wall;
   wall *second = new wall;
@@ -212,29 +258,47 @@ int main(){
   second->way = true;
   second->start_x = 1;
   second->end_x = 39;
-  second->start_y = 13;
+  second->start_y = 10;
   second->start_door = 25;
   second->end_door = 28;
 
   third->way = true;
   third->start_x = 1;
   third->end_x = 39;
-  third->start_y = 9;
+  third->start_y = 15;
   third->start_door = 15;
   third->end_door = 18;
 
-  head->next = second;
+  head->next = NULL;
   second->next = third;
   third->next = NULL;
 
+  room *room1 = new room;
+  room *room2 = new room;
+
+  room1->win = win;
+  room1->max_x = 40;
+  room1->max_y = 20;
+  room1->walls = head;
+  room1->start_door = 10;
+  room1->end_door = 13;
+  room1->prev = NULL;
+  room1->next = room2;
+
+  room2->win = win2;
+  room2->max_x = 40;
+  room2->max_y = 20;
+  room2->start_door = 33;
+  room2->end_door = 36;
+  room2->walls = second;
+  room2->prev = room1;
+  room2->next = NULL;
+
   refresh();
-  box(win, 0, 0);
-  // wborder(win2, 0, 0, 0, 0, 0, 0, 0, 0);
-  wrefresh(win);
 
   stampa_muri(head, win);
 
-  Personaggio *fc = new Personaggio(win, 20, 40, 1, 1);
+  Personaggio *fc = new Personaggio(room1, 20, 40);
 
   while(true){
     int c = getch();
@@ -246,8 +310,11 @@ int main(){
       default: break;
     }
     if(c == 'l'){
-      delwin(win);
-      touchwin(stdscr);
+      // delwin(win);
+      // touchwin(stdscr);
+      // clear();
+      // refresh();
+      stampa_muri(second, win2);
       break;
     }
   }
